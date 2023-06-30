7cd4010faf762f0dfd3456bef5e0a382e113bd3d
Claudia <135162896+claudiad4@users.noreply.github.com>
2023-06-30 16:31:25 +0200

    Add files via upload

---
+++ Zadanie_1/Zadanie_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Inicjalizacja tablicy 10x10
+        int[,] tablica = new int[10, 10];
+
+        // Wypełnianie tablicy
+        int liczba = 1;
+        for (int i = 0; i < 10; i++)
+        {
+            if (i % 2 == 0)
+            {
+                // Parzyste wiersze (rosnąco)
+                for (int j = 0; j < 10; j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+            else
+            {
+                // Nieparzyste wiersze (malejąco)
+                for (int j = 0; j < 10; j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba--;
+                }
+                liczba += 10; // Przesunięcie liczby na początek następnego wiersza
+            }
+        }
+
+        // Wypisanie tablicy na ekranie
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Zadanie_3.1/Zadanie_3.1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int size;
+
+        // Pobieranie rozmiaru tablicy od użytkownika i sprawdzanie poprawności danych
+        do
+        {
+            Console.Write("Proszę o podanie rozmiaru tablicy: ");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out size))
+            {
+                Console.WriteLine("Niepoprawne dane. Wprowadź liczbę całkowitą.");
+                continue;
+            }
+
+            if (size <= 0)
+            {
+                Console.WriteLine("Rozmiar tablicy musi być większy od zera.");
+                continue;
+            }
+
+            break;
+        } while (true);
+
+        // Inicjalizacja tablicy
+        int[,] tablica = new int[size, size];
+
+        // Wypełnianie tablicy
+        int liczba = 1;
+        for (int i = 0; i < size; i++)
+        {
+            if (i % 2 == 0)
+            {
+                // Parzyste wiersze (rosnąco)
+                for (int j = 0; j < size; j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba++;
+                }
+            }
+            else
+            {
+                // Nieparzyste wiersze (malejąco)
+                for (int j = 0; j < size; j++)
+                {
+                    tablica[i, j] = liczba;
+                    liczba--;
+                }
+                liczba += size; // Przesunięcie liczby na początek następnego wiersza
+            }
+        }
+
+        // Wypisanie tablicy na ekranie
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(tablica[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
